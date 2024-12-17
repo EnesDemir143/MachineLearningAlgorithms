0f8fa5cd2025aa2f4678f4ae153ea86eccac9749
EnesDemir143 <enesd41@outlook.com>
2024-12-18 02:18:21 +0300

    Logistic regression için veri okunması data baseden.

---
+++ create.sql
@@ -0,0 +1,22 @@
+/* CREATE DATABASE diabates;
+
+USE diabates;
+
+CREATE TABLE diabetes_data (
+    Pregnancies INT,
+    Glucose INT,
+    BloodPressure INT,
+    SkinThickness INT,
+    Insulin INT,
+    BMI FLOAT,
+    DiabetesPedigreeFunction FLOAT,
+    Age INT,
+    Outcome INT
+); */
+/* LOAD DATA LOCAL INFILE '/Users/enesdemir/Desktop/MachineLearningAlgorithms/DataSets/diabetes.csv' 
+INTO TABLE diabetes_data 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS; */
+
